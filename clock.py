cf47b79c79a8ed72488dd24e408c5459bb9f92c7
Victor Yu <vcyu08@gmail.com>
2020-06-15 09:26:13 -0400

    fix timezone

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from automate_algo_trading import pairs_trading_algo
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=9, minute=57)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=9, minute=57, timezone='US/Eastern')
 def timed_job():
 	print("Start Pairs Trading Algo...")
 	pairs_trading_algo()
